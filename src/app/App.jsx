5cea965ef8018649003e1d63dfb294e254b36c60
유경아 <ukyunga0.1@gmail.com>
2026-03-18 01:15:31 +0900

    feat: MovieBanner 위젯 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,8 @@
 import Loading from '@/shared/ui/Loading';
 import { usePopularAll } from '@/entities/movie/api/usePopularAll';
 import Card from '../entities/Card';
-import { useMovieDetail } from '../entities/movie/api/useMovieDetail';
+import { useMovieDetail } from '@/entities/movie/api/useMovieDetail';
+import MovieBanner from '@/widgets/MovieBanner';
 
 function App() {
     const { all, isPending } = usePopularAll();
@@ -14,6 +15,7 @@ function App() {
 
     return (
         <div>
+            <MovieBanner item={all?.[0]} fade={true} />
             {all.map((item) => (
                 <Card key={item.id} item={item}></Card>
             ))}
